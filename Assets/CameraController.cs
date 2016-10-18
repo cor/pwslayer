09934b11e18cd45c234d58d259313e784422ed56
cor <cor@pruijs.nl>
2016-10-18 16:35:49 +0200

    Allow player to move camera using arrow keys

---
+++ Assets/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public float cameraMoveSpeedMultiplier;
+
+	// Update is called once per frame
+	void Update () {
+		float x = transform.position.x + (Input.GetAxis ("Horizontal") * cameraMoveSpeedMultiplier);
+		float y = transform.position.y + (Input.GetAxis ("Vertical") * cameraMoveSpeedMultiplier);
+		transform.position = new Vector3 (x, y, transform.position.z);
+	}
+}
