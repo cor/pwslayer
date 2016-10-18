af1d195d084e37c3e90c156499dfbb6a49d7ff1c
cor <cor@pruijs.nl>
2016-10-18 19:53:40 +0200

    Remove redundant code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 
