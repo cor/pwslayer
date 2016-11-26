ade0d3412495a1202ebdf9a1cc236dd57aaa8e65
cor <cor@pruijs.nl>
2016-11-26 12:59:05 +0100

    Add pinch to zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,21 +4,50 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-		public float dampTime;
-		private Vector3 velocity = Vector3.zero;
-		public Transform target;
+	// Pinch to zoom
+	public float zoomSpeed = .5f;
 
-		// Update is called once per frame
-		void LateUpdate () 
-		{
+	public float dampTime;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
 
-			if (target) {
-				Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-				Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); 
-				Vector3 destination = transform.position + delta;
-				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-			}
+
+
+	void Update() {
+	
+		if (Input.touchCount == 2) {
+			Touch touchZero = Input.GetTouch (0);
+			Touch touchOne = Input.GetTouch (1);
+
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+			float prevTouchDeltaMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+			float deltaMagnitudediff = prevTouchDeltaMagnitude = touchDeltaMag;
+
+			Camera cam = GetComponent<Camera> ();
+			cam.orthographicSize += deltaMagnitudediff * zoomSpeed;
+			cam.orthographicSize = Mathf.Max (cam.orthographicSize, .1f);
 
 		}
+	}
+
+
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+
+		if (target) {
+			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); 
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
+	}
+
+
 
 }
