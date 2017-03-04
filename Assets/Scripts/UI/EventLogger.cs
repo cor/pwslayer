4cd6d3b3ec681fe5284edde0de78ae639ea362bc
boneshake <stefan8898@hotmail.com>
2017-03-04 17:35:37 +0100

    responsive font size added

--- Assets/Scripts/UI/EventLogger.cs
+++ Assets/Scripts/UI/EventLogger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EventLogger : MonoBehaviour {
 
 	private GUIStyle guiStyle = new GUIStyle();
-	public int fontSize = new int();
+	private int fontSize = new int();
 	public int maxLines = new int();
 	private Queue<string> queue = new Queue<string>();
 	private string eventLog = "";
@@ -30,7 +30,7 @@ public class EventLogger : MonoBehaviour {
         }
 		
 	void OnGUI () {
-		guiStyle.fontSize = fontSize;
+		guiStyle.fontSize = Mathf.RoundToInt(Screen.height*0.021f);
 		guiStyle.normal.textColor = Color.white;	
 		GUI.Label(new Rect(5, (Screen.height - (0.3f*Screen.height)),0.6f*Screen.width,0.2f*Screen.height), eventLog, guiStyle);
 	}
