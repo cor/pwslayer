ff9009965c587645cb30513db4b75bc690a14625
boneshake <stefan8898@hotmail.com>
2017-03-04 17:04:10 +0100

    Simple GUI label with cube to test

---
+++ Assets/Scripts/UI/EventLogger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventLogger : MonoBehaviour {
+
+	private GUIStyle guiStyle = new GUIStyle();
+	public int fontSize = new int();
+	public int maxLines = new int();
+	private Queue<string> queue = new Queue<string>();
+	private string eventLog = "";
+
+	
+	public void ToLog(string activity) {
+			if (queue.Count >= maxLines){
+				queue.Dequeue();
+				queue.Enqueue(activity);
+				eventLog = "";
+				Debug.Log("remove");
+			}
+			else{
+				queue.Enqueue(activity);
+				eventLog = "";
+				Debug.Log("add");
+			}
+			foreach (string i in queue){
+					eventLog = eventLog + i + "\n";
+			}
+		
+        }
+		
+	void OnGUI () {
+		guiStyle.fontSize = fontSize;
+		guiStyle.normal.textColor = Color.white;	
+		GUI.Label(new Rect(5, (Screen.height - (0.3f*Screen.height)),0.6f*Screen.width,0.2f*Screen.height), eventLog, guiStyle);
+	}
+}
