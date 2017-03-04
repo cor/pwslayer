c8bb6da2d24ca2a7f1a363ab77d8865a76b790f3
cor <cor@pruijs.nl>
2017-03-04 21:18:31 +0100

    Disallow movement to dropped item tiles

--- Assets/Scripts/Level Generation/Level.cs
+++ Assets/Scripts/Level Generation/Level.cs
@@ -23,14 +23,13 @@ public class Level : MonoBehaviour {
 	public List<EnemyDefinition> enemyDefinitions = new List<EnemyDefinition>();
 
 
-	public GameObject voidTile;
 	public GameObject groundTile;
-	public GameObject wallTile;
-	public GameObject doorTile;
 	public GameObject slime;
+	public GameObject droppedItem;
 
 	string[,] tiles;
 	List<GameObject> enemies = new List<GameObject>();
+	List<GameObject> droppedItems = new List<GameObject>();
 
 
 	// Constraints
@@ -45,6 +44,9 @@ public class Level : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Generate ();
+		GameObject dropItem = Instantiate(droppedItem, Vector3.zero, Quaternion.identity);
+		dropItem.GetComponent<DroppedItem>().Init(3, new Position(50, 50));
+		droppedItems.Add(dropItem);
 	}
 
 	public void Generate() {
@@ -492,6 +494,16 @@ public class Level : MonoBehaviour {
 	void AddChestToTiles(Chest chest) {
 		tiles[chest.position.x, chest.position.y] = "chest";
 	}
+	
+	DroppedItem DroppedItemAtPosition(Position position) {
+		foreach (GameObject droppedItem in droppedItems) {
+			Position droppedItemPosition = droppedItem.GetComponent<DroppedItem>().position;
+			if (position.x == droppedItemPosition.x && position.y == droppedItemPosition.y) {
+				return droppedItem.GetComponent<DroppedItem>();
+			}	
+		}
+		return null;
+	}
 
 
 	void AddUsedOpeningToTiles(Opening usedOpening) {
@@ -664,7 +676,7 @@ public class Level : MonoBehaviour {
 	}
 		
 	public bool CanMoveToTile(Position position) {
-		return tiles [position.x, position.y] == "ground";
+		return tiles [position.x, position.y] == "ground" && DroppedItemAtPosition(position) == null;
 	}
 
 	public GameObject EnemyIsOnTile(Position position) {
