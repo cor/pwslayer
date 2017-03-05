c6c40cc99dc7724a7426613bc1bc1a185d7f0b5d
boneshake <stefan8898@hotmail.com>
2017-03-05 22:23:07 +0100

    im retarded its working now

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -6,6 +6,6 @@ public class HealthBar : MonoBehaviour {
 
 	public float HealthRemaining;
 	void Update(){
-		transform.localScale = new Vector3((float)HealthRemaining,1,1);
+		transform.localScale = new Vector3(HealthRemaining,1f,1f);
 	}
 }
