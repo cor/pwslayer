6c7b10de3eab052d9f732e1a1772015918ac073f
cor <cor@pruijs.nl>
2017-03-13 22:26:59 +0100

    Make health display work

---
+++ Assets/StatUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatUpdater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Text text = GetComponent<Text>();
+		int health = GameObject.FindWithTag("Player").GetComponent<Player>().health;
+		text.text = "HP: " + health + "/100";
+		
+	}
+}
